c0ad6d38a45da32222bf57ccfe8911b5f8701013
Daniel <danibarrantesv33@gmail.com>
2025-09-15 14:49:12 -0600

    ready

--- tiendaGBA/assets/js/components/header.js
+++ tiendaGBA/assets/js/components/header.js
@@ -3,16 +3,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function detectDevice() {
         const width = window.innerWidth;
+        const currentPath = window.location.pathname;
+        let basePath = '/tiendaGBA/includes/components/';
+
+        // Ajusta la ruta base si estamos en una subcarpeta
+        if (currentPath.includes('/pages/auth/')) {
+            basePath = '../../includes/components/';
+        } else if (currentPath.includes('/pages/')) {
+            basePath = '../includes/components/';
+        }
 
         // Mostrar según dispositivo
         if (width <= 768) {
-            fetch('/tiendaGBA/includes/components/header-mobile.html')
+            fetch(basePath + 'header-mobile.html')
                 .then(response => response.text())
                 .then(data => {
                     document.getElementById('header-container').innerHTML = data;
                 });
         } else {
-            fetch('/tiendaGBA/includes/components/header-mobile.html')
+            fetch(basePath + 'header-desktop.html')
                 .then(response => response.text())
                 .then(data => {
                     document.getElementById('header-container').innerHTML = data;
