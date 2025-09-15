545ac29017010e7093a0be3f9b2267e20620e43d
Daniel <danibarrantesv33@gmail.com>
2025-09-15 12:48:42 -0600

    order-files

---
+++ themes/js/jason.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOM está listo');
+
+    function detectDevice() {
+        const width = window.innerWidth;
+
+        // Mostrar según dispositivo
+        if (width <= 768) {
+            fetch('../view/Global/header-mobile.html')
+                .then(response => response.text())
+                .then(data => {
+                    document.getElementById('header-container').innerHTML = data;
+                });
+        } else {
+            fetch('../view/Global/header-desktop.html')
+                .then(response => response.text())
+                .then(data => {
+                    document.getElementById('header-container').innerHTML = data;
+                });
+        }
+    }
+
+    detectDevice();
+    window.addEventListener('resize', detectDevice);
+});
